20bec198c05cb5b35aa9c06cb52d047850c6dc1e
Cihad GÜNDOĞDU <cihadgundogdu@gmail.com>
2024-02-28 16:18:10 +0300

    Initial Commit

--- src/main/kotlin/com/turbohesap/NetloginApplication.kt
+++ src/main/kotlin/com/turbohesap/NetloginApplication.kt
@@ -2,8 +2,13 @@ package com.turbohesap
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
+import org.springframework.context.annotation.PropertySources
 
 @SpringBootApplication
+@PropertySources(
+    PropertySource("classpath:application-\${spring.profiles.active:prod}.properties")
+)
 class NetloginApplication
 
 fun main(args: Array<String>) {
